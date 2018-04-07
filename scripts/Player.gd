c6ca6e203787a2f2746c6ff8e00856956689b536
Philip Abernethy <chais.z3r0@gmail.com>
2018-04-06 21:32:57 -0400

    Added basic controls to Player
    Player can now walk left and right, and jump.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,15 +1,23 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+export(float) var gravity = 9.8
+export(float) var walkSpeed = 200
+export(float) var jumpHeight = 300
+
+var velocity = Vector2()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _physics_process(delta):
+	velocity.y += gravity
+	var walk = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		walk -= walkSpeed
+	if Input.is_action_pressed("ui_right"):
+		walk += walkSpeed
+	velocity.x = walk
+	if is_on_floor() && Input.is_action_pressed("ui_up"):
+		velocity.y += -jumpHeight
+	velocity = move_and_slide(velocity, Vector2(0, -1))
