2ebe8667612bcee957a42649f6fe28116d7248d3
Philip Abernethy <chais.z3r0@gmail.com>
2018-04-09 18:48:25 -0400

    Added basic whip

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -17,11 +17,16 @@ func _physics_process(delta):
 		velocity.y += gravity
 
 	var walk = 0
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("game_left"):
 		walk -= walkSpeed
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("game_right"):
 		walk += walkSpeed
 	velocity.x = walk
-	if is_on_floor() && Input.is_action_pressed("ui_up"):
-		velocity.y += -jumpSpeed
+	if is_on_floor() && Input.is_action_pressed("game_jump"):
+		velocity.y -= jumpSpeed
+	if Input.is_action_just_pressed("game_whip"):
+		var whip = get_node("Whip")
+		var pointer = get_viewport().get_mouse_position() - position - get_node("Whip").position
+		whip.rotation = whip.get_angle_to(get_viewport().get_mouse_position())
+		whip.whip()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
